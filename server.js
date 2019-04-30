3fce26e8683e6735a33de6d0786c977decb8955f
arslanfareed <arslan_fareed05@live.com>
2019-05-01 02:06:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -222,6 +222,157 @@ if(emailverification.email != email ){
 
 });
 
+app.get('/formSubmit', function(req, res) {
+ 
+  var state = req.query.state;
+  var localGovt = req.query.localGovt;
+  var disability = req.query.disability;
+  var schoolName = req.query.schoolName;
+  var dob = req.query.dob;
+  var coreSubject1 = req.query.coreSubject1;
+  var coreSubject2 = req.query.coreSubject2;
+  var coreSubject3 = req.query.coreSubject3;
+  var es1 = req.query.es1;
+  var es2 = req.query.es2;
+  var es3 = req.query.es3;
+  var es4 = req.query.es4;
+  var es5 = req.query.es5;
+  var es6 = req.query.es6;
+  var s11 = req.query.s11;
+  var s12 = req.query.s12;
+  var s13 = req.query.s13;
+  var s14 = req.query.s14;
+  var s15 = req.query.s15;
+  var s16 = req.query.s16;
+  var s17 = req.query.s17;
+  var s18 = req.query.s18;
+  var s19 = req.query.s19;
+  var s21 = req.query.s21;
+  var s22 = req.query.s22;
+  var s23 = req.query.s23;
+  var s24 = req.query.s24;
+  var s25 = req.query.s25;
+  var s26 = req.query.s26;
+  var s27 = req.query.s27;
+  var s28 = req.query.s28;
+  var s29 = req.query.s29;
+  var s31 = req.query.s31;
+  var s32 = req.query.s32;
+  var s33 = req.query.s33;
+  var s34 = req.query.s34;
+  var s35 = req.query.s35;
+  var s36 = req.query.s36;
+  var s37 = req.query.s37;
+  var s38 = req.query.s38;
+  var s39 = req.query.s39;
+  var email = req.query.email;
+  
+
+
+  if (!db) {
+    initDb(function(err){});
+  }
+ 
+  if (db) {
+
+  var myobj = 
+    { state: state,
+      localGovt: localGovt, 
+      disability: disability, 
+      schoolName: schoolName,
+      dob: dob,
+      coreSubject1: coreSubject1,
+      coreSubject2: coreSubject2,
+      coreSubject3: coreSubject3,
+      es1: es1,
+      es2: es2,
+      es3: es3,
+      es4: es4,
+      es5: es5,
+      es6: es6, 
+      s11: s11,
+      s12: s12,
+      s13: s13, 
+      s14: s14,
+      s15: s15,
+      s16: s16,
+      s17: s17,
+      s18: s18,
+      s19: s19,
+      s21: s21,
+      s22: s22,
+      s23: s23,
+      s24: s24,
+      s25: s25,
+      s26: s26,
+      s27: s27,
+      s28: s28,
+      s29: s29,
+      s31: s31,
+      s32: s32,
+      s33: s33,
+      s34: s34,
+      s35: s35,
+      s36: s36,
+      s37: s37,
+      s38: s38,
+      s39: s39,
+      email: email,
+      submission: "submitted" };
+  db.collection("forms").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 form inserted");
+    console.log(req.query.schoolName)
+    console.log(schoolName);
+    
+  });
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers Authorization, Autho, X-Requested-With');
+  res.send('{"form":"inserted"}');
+};
+  
+});
+
+
+
+app.get('/checkSubmission', function(req, res) {
+  var email = req.query.email;
+  var data = "";
+  if (!db) {
+    initDb(function(err){});
+  }
+ 
+  if (db) {
+  console.log(email);
+  db.collection("forms").findOne({email:email},{}, function(err, result) {
+    if (err) throw err;
+    if(result){
+    data = result;
+    console.log(result);
+    console.log("Form Already Submitted");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers Authorization, Autho, X-Requested-With');
+//  res.send("Hellow World");
+    res.send(JSON.stringify(data));
+    
+    }
+    else{
+      data = {submission:"not submitted"};
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers Authorization, Autho, X-Requested-With');
+//  res.send("Hellow World");
+      res.send(JSON.stringify(data));
+      console.log("Form is not Submitted");
+      
+    }
+    
+  });
+};
+//  res.send(userName);
+
+
+});
 
 // error handling
 app.use(function(err, req, res, next){
